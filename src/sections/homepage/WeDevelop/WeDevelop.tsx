5d9ca8a7795ba4dc54789dd8c90c690912cf1f4f
angel <angelpublicista.1@gmail.com>
2024-04-14 15:39:30 -0500

    feat: start responsive wedevelop

--- src/sections/homepage/WeDevelop/WeDevelop.tsx
+++ src/sections/homepage/WeDevelop/WeDevelop.tsx
@@ -4,15 +4,15 @@ import Isologo from '../../../../public/assets/images/Isologo Geniorama - otros 
 
 export default function WeDevelop() {
   return (
-    <section className="py-28 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 pb-48">
-      <div className="container relative pt-16 pb-20">
-        <div className="flex items-center justify-between relative z-10">
-          <div className="w-1/2">
+    <section className="pt-28 pb-0 md:pb-64 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 pb-48">
+      <div className="container relative pt-16 pb-16 md:pb-20">
+        <div className="px-5 md:px-0 md:flex items-center justify-between relative z-10">
+          <div className="md:w-1/2">
             <h2 className={`${styles.title}`} style={{ color: "white" }}>
-              /We Develop.
+              /We <span>Develop.</span>
             </h2>
           </div>
-          <div className="w-1/3">
+          <div className="md:w-1/3">
             <p className={`${styles.desc}`}>
               Yes, that’s our mantra but also our every-day work. We are a group
               of obsessed code-folks who make incredible software, apps and
@@ -23,9 +23,9 @@ export default function WeDevelop() {
           </div>
         </div>
 
-        <div className="w-full absolute top-0 left-0 d-flex justify-center text-center items-center">
-            <img className="w-full max-w-screen-lg m-auto rounded-lg" src={Cover.src} alt="" />
-            <img className="absolute" style={{ bottom: '-19.1%', right: '10.1%', width: '200px'}} src={Isologo.src} alt="" />
+        <div className="w-full absolute top-0 left-1/4 md:left-0 d-flex justify-center text-center items-center">
+            <img className="w-full md:max-w-screen-lg m-auto md:rounded-lg" src={Cover.src} alt="" />
+            <img className={`${styles.isologo} absolute`} src={Isologo.src} alt="" />
         </div>
       </div>
     </section>
