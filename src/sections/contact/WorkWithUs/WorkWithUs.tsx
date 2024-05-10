99a97585fe1d3dc78c0ae24bc7d3571bb0e0f8f8
angelpublicista <angelpublicista.1@gmail.com>
2024-05-09 21:25:31 -0500

    feat: contact page form

--- src/sections/contact/WorkWithUs/WorkWithUs.tsx
+++ src/sections/contact/WorkWithUs/WorkWithUs.tsx
@@ -1,14 +1,19 @@
-import React from "react";
+import ContactForm from "@/components/ContactForm/ContactForm";
 
 export default function WorkWithUs() {
   return (
-    <section className="py-56 text-white">
+    <section className="pb-20 md:py-56 text-white">
       <div className="container">
         <div className="px-3 md:max-w-4xl md:mx-auto">
-          <div>
+          <div className="relative">
+            <span style={{left: '-50px', fontFamily: 'Poppins'}} className="font-light text-8xl absolute top-3">/</span>
             <p className="font-light text-4xl leading-none">
                 Want to <br /> work with <br /> geniuses?
             </p>
+          </div>
+
+          <div className="mt-10">
+            <ContactForm />
           </div>
         </div>
       </div>
